d658080b581234fb1b233889c7141fd6b52fda89
Antonio Jennings <Jenningsantonio17@gmail.com>
2020-12-07 09:20:17 -0500

    Finished Final Project

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php   
+    include './View/header.php';
+    include './View/navbar.php';
+
+?>
+
+        <div class="info-grid">
+            <img class="bare-bears" src='./View/Public/Images/weBareBears.png'> 
+            <h2 class="my-info">Hello there! My name is Antonio and I'm a Web Developer!</h2>
+            <p class="my-info">I've worked with HTML/CSS, Ajax, Php, and Javascript</p>
+        </div>
+
+
+
+
+<!--   | web grid end -->
+<!--   V                -->
+    </div> 
---
+++ projects.php
@@ -0,0 +1,21 @@
+<?php   
+    include './View/header.php';
+    include './View/navbar.php';
+
+?>
+
+<script>
+    document.getElementById('background').style.background = "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('./View/Public/Images/LightJar.jpg')";
+    document.getElementById('background').style.backgroundPosition = "center"; 
+    document.getElementById('background').style.backgroundRepeat = "no-repeat";
+    document.getElementById('background').style.backgroundSize = "cover"; 
+</script>
+
+    <!-- Iframe Grid -->
+        <div class="iframe-grid">
+            <a href='./View/Public/Projects/FIT-Project--Learning-Experience-/index.php' target="_blank"><h2 style="background-color: black; color: white;">Learning Experience</h2></a>
+            <a href='./View/Public/Projects/Bubble-Blower/index.php' target="_blank"><h2 style="background-color: black; color: white;">Bubble Blower</h2></a>
+            <a href='./View/Public/Projects/FIT--Web-Exercises-Week-01/Index.html' target="_blank"><h2 style="background-color: black; color: white;">Anime Phone Case</h2></a>
+        </div>
+</body>
+</html>
